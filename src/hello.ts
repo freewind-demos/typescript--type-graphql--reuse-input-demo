f2b331cdc9f46c081991da82a06c7169e5ca6eb5
Freewind <nowindlee@gmail.com>
2020-05-05 22:37:21 +0800

    improve

--- src/hello.ts
+++ src/hello.ts
@@ -1,21 +1,16 @@
 import {graphql} from 'graphql';
-import 'reflect-metadata';
-import {buildSchema} from 'type-graphql';
 import path from 'path';
+import 'reflect-metadata';
+import {buildSchemaSync} from 'type-graphql';
 
 import UiQueryResolver from './UiQueryResolver';
 
-async function main() {
-  const schema = await buildSchema({
-    resolvers: [UiQueryResolver],
-    emitSchemaFile: path.resolve(__dirname, '../dist/schema.graphql'),
-    validate: false // https://github.com/MichalLytek/type-graphql/issues/150
-  });
+const schema = buildSchemaSync({
+  resolvers: [UiQueryResolver],
+  emitSchemaFile: path.resolve(__dirname, '../dist/schema.graphql'),
+  validate: false // https://github.com/MichalLytek/type-graphql/issues/150
+});
 
-  graphql(schema, '{ standardize(uiQuery: { field1: " aa ", field2: "  bb "}) { field1, field2 }}').then(result => {
-    console.log(JSON.stringify(result));
-  });
-
-}
-
-main();
+graphql(schema, '{ standardize(uiQuery: { field1: " aa ", field2: "  bb "}) { field1, field2 }}').then(result => {
+  console.log(JSON.stringify(result));
+});
